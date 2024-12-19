04763541c776874a7f3d08c8885807a6f428b127
luckyduckys <mechammitchell@outlook.com>
2024-12-19 08:54:42 -0700

    Add .gitignore

--- I am Rich/ContentView.swift
+++ I am Rich/ContentView.swift
@@ -10,12 +10,22 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack {
+                Text("I am Rich")
+                    .font(.largeTitle)
+                    .foregroundStyle(Color.white)
+                    .frame(maxWidth: .infinity)
+            }
+            .background(Color(red: 0.145, green: 0.176, blue: 0.2))
+            .shadow(radius: 10)
+            .frame(width: .infinity)
+            Spacer()
+            Image("Diamond")
+                .resizable()
+                .frame(width: 300, height: 300)
+            Spacer()
         }
-        .padding()
+        .background(Color(red: 0.349, green: 0.447, blue: 0.494))
     }
 }
 
